f9c0500117f449db427a0119c19861eddb859182
saber sun <sbhbcf@163.com>
2018-05-29 15:20:42 +0800

    疑似追踪接口

--- src/main/java/com/ai/oidd/pt/biz/UserAffectedBiz.java
+++ src/main/java/com/ai/oidd/pt/biz/UserAffectedBiz.java
@@ -1,14 +1,18 @@
 package com.ai.oidd.pt.biz;
 
+import com.ai.oidd.pt.common.msg.TableResultResponse;
+import com.ai.oidd.pt.entity.TerminalSuspStats;
 import com.ai.oidd.pt.entity.UserAffected;
 import com.ai.oidd.pt.mapper.UserAffectedMapper;
-import com.ai.oidd.pt.vo.CityCodeQty;
-import com.ai.oidd.pt.vo.CommonQty;
-import com.ai.oidd.pt.vo.DateQty;
-import com.ai.oidd.pt.vo.MdnQty;
+import com.ai.oidd.pt.vo.*;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -74,5 +78,78 @@ public class UserAffectedBiz extends BaseBiz<UserAffectedMapper, UserAffected> {
      */
     public List<DateQty> countAffectedByTimestamp(@Param("start") Integer start, @Param("end") Integer end) {
         return mapper.countAffectedByTimestamp(start, end);
+    }
+
+    /**
+     * 按条件查询
+     *
+     * @param vo
+     * @return
+     */
+    public TableResultResponse<UserAffected> queryByExampleAndPage(UserAffectedVo vo) {
+        Example example = new Example(TerminalSuspStats.class);
+        Example.Criteria criteria = example.createCriteria();
+        boolean isCriteria = false;
+        int page = 1;
+        int limit = 10;
+        if (null != vo) {
+            String mdn = vo.getMdn();
+            if (StringUtils.isNotEmpty(mdn)) {
+                isCriteria = true;
+                criteria.andEqualTo("mdn", mdn);
+            }
+
+            String sourceId = vo.getSourceId();
+            if (StringUtils.isNotEmpty(sourceId)) {
+                isCriteria = true;
+                criteria.andEqualTo("sourceId", sourceId);
+            }
+
+            String baseId = vo.getBaseId();
+            if (StringUtils.isNotEmpty(baseId)) {
+                isCriteria = true;
+                criteria.andEqualTo("baseId", baseId);
+            }
+
+            String type = vo.getType();
+            if (StringUtils.isNotEmpty(type)) {
+                isCriteria = true;
+                criteria.andEqualTo("type", type);
+            }
+
+            Date start = vo.getStartTime();
+            Date end = vo.getEndTime();
+            if (null != start && null != end) {
+                isCriteria = true;
+                criteria.andBetween("time", start, end);
+            }
+
+            Date occurTimeStart = vo.getOccurTimeStart();
+            Date occurTimeEnd = vo.getOccurTimeEnd();
+            if (null != occurTimeStart && null != occurTimeEnd) {
+                isCriteria = true;
+                criteria.andBetween("occurTime", occurTimeStart, occurTimeEnd);
+            }
+
+            page = vo.getPage();
+            limit = vo.getLimit();
+        }
+
+
+        if (0 == page && 0 == limit) {
+            page = 1;
+            limit = 10;
+        }
+
+        Page<Object> result = PageHelper.startPage(page, limit);
+
+        List<UserAffected> list;
+        if (isCriteria) {
+            list = mapper.selectByExample(example);
+        } else {
+            list = mapper.selectByExample(new Example(UserAffected.class));
+        }
+
+        return new TableResultResponse<>(result.getTotal(), list);
     }
 }
